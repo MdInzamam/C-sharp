b229c9cca86d3b99e35c9436a5a6d8e0996980f5
MdInzamam <87995678+MdInzamam@users.noreply.github.com>
2023-11-04 03:21:45 +0530

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class account
+    {
+        public int accountBalance = 1000;
+        //int accountBalance = 1000;
+
+        //public void setBalance(int Amount)
+        //{
+        //    accountBalance = Amount;
+        //}
+
+        //public void getBalance() {
+        //    Console.WriteLine("Your account balance is : " + accountBalance);
+        //}
+        public int Balance
+        {
+            set
+            {
+                accountBalance = value;
+            }
+            get
+            {
+                return accountBalance;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            account myAccount = new account();
+            //myAccount.accountBalance = 100; 
+            //myAccount.setBalance(10000);
+            //myAccount.getBalance();
+
+            myAccount.Balance = 10000;
+            Console.WriteLine("Your account balance is : " + myAccount.Balance);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ find Vowel consonant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_Vowel_consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "AbCdef";
+            string small = str.ToLower();
+            int vowel = 0;
+            int consonant = 0;
+            foreach (char character in small)
+            {
+                if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+                {
+                    vowel++;
+                }
+                else
+                {
+                    consonant++;
+                }
+            }
+            Console.WriteLine("Vowel"+ vowel);
+            Console.WriteLine("Consonant"+consonant);
+            Console.ReadLine();
+        }
+    }
+}
